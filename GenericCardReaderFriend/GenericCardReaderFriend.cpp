162b03c661f201f081ad62fcbd36cc86cf1e159f
FireWolf <austere.j@gmail.com>
2021-08-06 13:57:11 -0700

    Set the minimum kernel version to El Capitan.

--- GenericCardReaderFriend/GenericCardReaderFriend.cpp
+++ GenericCardReaderFriend/GenericCardReaderFriend.cpp
@@ -208,7 +208,7 @@ PluginConfiguration ADDPR(config) =
     arrsize(bootargDebug),
     bootargBeta,
     arrsize(bootargBeta),
-    KernelVersion::Mojave,
+    KernelVersion::ElCapitan,
     KernelVersion::Monterey,
     start
 };
